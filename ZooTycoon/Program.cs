a652edce601009b9458de1f49364e8a85c3efd3b
Julien Groll <julien.groll@gmail.com>
2017-09-17 17:36:03 +0200

    Add IHM

--- ZooTycoon/Program.cs
+++ ZooTycoon/Program.cs
@@ -23,32 +23,17 @@ namespace ZooTycoon
 
         public static void Main(string[] args)
         {
-
-            Console.WriteLine("######### ZooTycoon ########");
             uow = new UnitOfWork();
             _homeController = new HomeController(uow);
             _animalController = new AnimalController(uow);
             _clientController = new ClientController(uow);
 
             _homeController.initialisation();
-            _clientController.EntreZoo();
+            //_clientController.EntreZoo();
 
-            _animalController.GetAllHuitre().ForEach(x =>
-            {
-                Console.WriteLine(x.Cri());
-            });
+            menuHome();
 
-            _animalController.GetAllBaleine().ForEach(x =>
-            {
-                Console.WriteLine(x.Cri());
-            });
-
-            Console.WriteLine("Les enclos présents dans le zoo sont les suivants :");
-            _animalController.GetAllEnclos().ForEach(x =>
-            {
-                _animalController.DescriptionEnclos(x);
-            });
-
+ /*
             Console.WriteLine("Les spectacles présents dans le zoo sont les suivants :");
             _animalController.GetAllSpectacles().ForEach(x =>
             {
@@ -63,15 +48,149 @@ namespace ZooTycoon
                 StartSpectacle(_animalController.GetAllSpectacles().First());
             });
 
+           Console.ReadLine(); 
+ */
+        }
+
+        public static void menuHome()
+        {
+            Console.Clear();
+            string res;
+            Console.WriteLine("#######################################################################################################################");
+            Console.WriteLine("Bienvenue dans ZooTycoon");
+            Console.WriteLine("Etes-vous prêt pour du GROS FUN ? ");
+            dino();
+            Console.WriteLine("");
+            Console.WriteLine("Veuillez selectionner un des choix suivant : ");
+            Console.WriteLine("1. Animaux");
+            Console.WriteLine("2. Spectacle");
+            Console.WriteLine("3. Magasin");
+            Console.WriteLine("#######################################################################################################################");
+            res = Console.ReadLine();
+            switch (res.ToString())
+            {
+                case "1":
+                    menuAnimaux();
+                    break;
+                case "2":
+                    menuSpectacle();
+                    break;
+                case "3":
+                    menuMagasin();
+                    break;
+                default:
+                    menuHome();
+                    break;
+            }
+        }
+        public static void menuAnimaux()
+        {
+            Console.Clear();
+            Console.WriteLine("#######################################################################################################################");
+            Console.WriteLine("Les enclos présents dans le zoo sont les suivants :");
+            _animalController.GetAllEnclos().ForEach(x =>
+            {
+                _animalController.DescriptionEnclos(x);
+            });
+            retourHome();
+            /*
+                        _animalController.GetAllHuitre().ForEach(x =>
+                        {
+                            Console.WriteLine(x.Cri());
+                        });
+                        _animalController.GetAllBaleine().ForEach(x =>
+                        {
+                            Console.WriteLine(x.Cri());
+                        });
+                        
+            */
+
+        }
+
+        public static void menuSpectacle()
+        {
+            Console.Clear();
+            Console.WriteLine("#######################################################################################################################");
+            Console.WriteLine("Les spectacles présents dans le zoo sont les suivants :");
+            _animalController.GetAllSpectacles().ForEach(x =>
+            {
+                _animalController.DescriptionSpectacle(x);
+            });
+            Console.WriteLine("1. Lancer un spectacle ");
+            string res = Console.ReadLine();
+            switch (res.ToString())
+            {
+                case "1":
+                    StartSpectacle(_animalController.GetAllSpectacles().First());
+                    break;
+                default:
+                    menuHome();
+                    break;
+            }
+        }
+
+        public static void menuMagasin()
+        {
+            string res;
+            Console.Clear();
+            Console.WriteLine("#######################################################################################################################");
+            magasin();
+            Console.WriteLine("Vers quel type de magasin voulez vous aller ? :");
+            Console.WriteLine("1. Magasin de Souvenir");
+            Console.WriteLine("2. Magasin de Nourriture");
+            Console.WriteLine("3. Magasin de Boisson");
+            Console.WriteLine("4. Retour Menu");
+            Console.WriteLine("#######################################################################################################################");
+            res = Console.ReadLine();
+            switch (res.ToString())
+            {
+                case "1":
+                    menuSouvenir();
+                    break;
+                case "2":
+                    menuNourriture();
+                    break;
+                case "3":
+                    menuBoisson();
+                    break;
+                case "4":
+                    menuHome();
+                    break;
+                default:
+                    menuMagasin();
+                    break;
+            }
             Console.ReadLine();
+        }
+
+        public static void menuSouvenir()
+        {
+            Console.Clear();
+            Console.WriteLine("#######################################################################################################################");
+            Console.WriteLine("Bienvenue dans le magasin de souvenir :");
+            retourHome();
+        }
+
+        public static void menuNourriture()
+        {
+            Console.Clear();
+            Console.WriteLine("#######################################################################################################################");
+            Console.WriteLine("Bienvenue dans le magasin de nourriture :");
+            retourHome();
+        }
+
+        public static void menuBoisson()
+        {
+            Console.Clear();
+            Console.WriteLine("#######################################################################################################################");
+            Console.WriteLine("Bienvenue dans le magasin de boisson :");
+            retourHome();
         }
 
         public static void StartSpectacle(Spectacle spectacle)
         {
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("**********************************");
+            Console.Clear();
+            Console.WriteLine("#######################################################################################################################");
             Console.WriteLine("Madame et Messieurs, bienvenue dans notre tout nouveau spectacle");
             _animalController.DescriptionSpectacle(spectacle);
 
@@ -83,6 +202,58 @@ namespace ZooTycoon
 
             Console.WriteLine("Je vous prie d'applaudir nos animateurs et nos animaux");
             Console.WriteLine(_animalController.DescriptionAnimateur(spectacle));
+            Console.WriteLine("#######################################################################################################################");
+            retourHome();
+        }
+        public static void retourHome()
+        {
+            Console.ReadLine();
+            menuHome();
+        }
+
+        public static void dino()
+        {
+            Console.WriteLine(@"                                    _
+                                  .~q`,
+                                 {__,  \
+                                     \' \
+                                      \  \
+                                       \  \
+                                        \  `._            __.__
+                                         \    ~-._  _.==~~     ~~--.._
+                                          \        '                  ~-.
+                                           \      _-   -_                `.
+                                            \    /       }        .-    .  \
+                                             `. |      /  }      (       ;  \
+                                               `|     /  /       (       :   '\
+                                                \    |  /        |      /       \
+                                                 |     /`-.______.\     |~-.      \
+                                                 |   |/           (     |   `.      \_
+                                                 |   ||            ~\   \      '._    `-.._____..----..___
+                                                 |   |/             _\   \         ~-.__________.-~~~~~~~~~'''
+                                               .o'___/            .o______}");
+        }
+
+        public static void magasin()
+        {
+            Console.WriteLine(@"                           ====
+                           !!!!
+      ==========================
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  ||      _____          _____    ||
+  ||      | | |          | | |    ||
+  ||      |-|-|          |-|-|    ||
+  ||      #####          #####    ||
+  ||                              ||
+  ||      _____   ____   _____    ||
+  ||      | | |   @@@@   | | |    ||
+  ||      |-|-|   @@@@   |-|-|    ||
+  ||      #####   @@*@   #####    ||
+  ||              @@@@            ||
+******************____****************
+**************************************");
         }
     }
 }
